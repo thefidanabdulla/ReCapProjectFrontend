c52320fc5c1c257504403715b91a60ce5e451249
thefidanabdulla <thefidanabdulla@gmail.com>
2022-06-25 19:34:51 +0400

    Angular beginner version for Rental Car Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ReCapProjectFrontend';
+  title: string = 'ReCapProjectFrontend';
+  user: string = "Fidan Abdulla";
+  product1: any = {
+    productId: 1,
+    productName: 'SmartPen',
+    categoryId: 1,
+    unitPrice: 25
+  };
+  product2: any = {
+    productId: 2,
+    productName: 'Laptop',
+    categoryId: 3,
+    unitPrice: 25
+  };
+  product3: any = {
+    productId: 3,
+    productName: 'Mouse',
+    categoryId: 1,
+    unitPrice: 25
+  };
+  product4: any = {
+    productId: 4,
+    productName: 'Keyboard',
+    categoryId: 1,
+    unitPrice: 25
+  };
+  product5: any = {
+    productId: 5,
+    productName: 'Camera',
+    categoryId: 1,
+    unitPrice: 25
+  };
+  products = [
+    this.product1,
+    this.product2,
+    this.product3,
+    this.product4,
+    this.product5
+  ]
 }
