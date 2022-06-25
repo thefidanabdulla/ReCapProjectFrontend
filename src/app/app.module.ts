b1c433cdc750036025fc4a86f4fb3fc9fff180ee
thefidanabdulla <thefidanabdulla@gmail.com>
2022-06-25 20:00:50 +0400

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { CarComponent } from './components/car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviComponent,
+    BrandComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule,
