270ca5f484558cba3dacf8ad13972ceb85cc7266
thefidanabdulla <thefidanabdulla@gmail.com>
2022-07-05 19:55:41 +0400

    color changes added

--- src/app/components/color/color.component.ts
+++ src/app/components/color/color.component.ts
@@ -10,6 +10,7 @@ import { ColorService } from 'src/app/services/color/color.service';
 export class ColorComponent implements OnInit {
 
   colors:Color[] = [];
+  currentColor:Color;
   dataLoaded = false;
 
   constructor(private colorService:ColorService) { }
@@ -22,6 +23,18 @@ export class ColorComponent implements OnInit {
       this.colors = response.data;
       this.dataLoaded = true;
     })
+  }
+
+  setCurrentBrand(color:Color){
+    this.currentColor = color;
+  }
+
+  getCurrentBrandClass(color:Color){
+    if(color == this.currentColor){
+      return 'list-group-item bg-dark text-white ';
+    }else{
+      return 'list-group-item ';
+    }
   }
 
 }
