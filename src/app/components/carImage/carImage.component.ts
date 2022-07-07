538ddf0de4acb8912b601dc49b67966286516ec1
thefidanabdulla <thefidanabdulla@gmail.com>
2022-07-07 22:42:19 +0400

    Camp 18. Day END

--- src/app/components/carImage/carImage.component.ts
+++ src/app/components/carImage/carImage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CarImage } from 'src/app/models/carImage/carImage';
+import { CarImageService } from 'src/app/services/carImage/carImage.service';
 
 @Component({
   selector: 'app-carImage',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarImageComponent implements OnInit {
 
-  constructor() { }
+  carImages:CarImage[] = [];
+  constructor(
+    private carImageService:CarImageService,
+    private activatedRoute:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params=>{
+      if(params["carId"]){
+        this.getCarImagesByCarId(params["carId"])
+      }
+    })
+  }
+  getCarImagesByCarId(carId:number){
+    this.carImageService.getCarImagesByCarId(carId).subscribe(response => {
+      this.carImages = response.data;
+      console.log(this.carImages)
+    })
   }
 
 }
